a94ddbaaf9cb9a6eb8e33e9b3260a86204a930ce
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-08-31 17:05:29 +0700

    -

--- StrukturKendali/l1.php
+++ StrukturKendali/l1.php
@@ -1,21 +1,21 @@
 <?php 
 //Looping : for, while, do.. while, Foreach (khusus Array)
 
-// for ( $i = 0; $i < 5; $i++ ) {
-//     echo "Hello World $i <br>";
-// }
+for ( $i = 0; $i < 5; $i++ ) {
+    echo "Hello World $i <br>";
+}
 
-// $i = 0;
-// while( $i < 5 ){
-//     echo "hello world $i <br> ";
-//     $i++;
-// }
+$i = 0;
+while( $i < 5 ){
+    echo "hello world $i <br> ";
+    $i++;
+}
 
 
-// $i = 0;
-// do {
-//     echo "hello world! $i <br>";
-//     $i++;
-// } while ( $i < 5 );
+$i = 0;
+do {
+    echo "hello world! $i <br>";
+    $i++;
+} while ( $i < 5 );
 
 ?>
