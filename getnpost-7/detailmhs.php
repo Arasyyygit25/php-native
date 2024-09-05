30c7092696bbfeda004d81daa3c96749674d5dd4
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-09-05 18:46:54 +0700

    redirect

--- getnpost-7/detailmhs.php
+++ getnpost-7/detailmhs.php
@@ -1,6 +1,15 @@
 <?php
-
-
+//gimana supaya user yang mencoba masuk ke latihan 2 itu tidak boleh ngetik langsung ke url, tapi harus lewat link pertama, anggap saja orang yang ngetik langsung di url itu adalah orang jahat yang mau masuk ke dalam sistem. jadi orang yang datang ke halaman 2 harus bawa data dari latihan 1 
+// cek apakah tidak ada data di $_GET
+ 
+if( !isset($_GET["nama"]) ||
+ !isset($_GET["nim"]) ||
+ !isset($_GET["email"] ||
+ !isset($_GET["jurusan"]))){ // jadi ketika block dibikin if not issect + variabel, belum dibikin kita lakukan sesuatu yaitu, kita paksa/pindahkan user untuk pindah dari hal 2 ke halaman 1 yaitu redirect., 
+    //redirect: memindahkan user dari sebuah halaman ke halaman lainnya
+    header("Location: index2.php");
+    exit; // supaya script yang bawah tidak di jalankan
+}
 
 ?>
 
@@ -23,7 +32,7 @@
     <li><?= $_GET["jurusan"]; ?></li>
 </ul>
 
-<!-- gimana supaya user yang mencoba masuk ke latihan 2 itu tidak boleh ngetik langsung ke url, tapi harus lewat link pertama, anggap saja orang yang ngetik langsung di url itu adalah orang jahat yang mau masuk ke dalam sistem. jadi orang yang datang ke halaman 2 harus bawa data dari latihan 1 -->
+
 <a href="index2.php">Kembali ke menu awal.</a>
 
 </body>
