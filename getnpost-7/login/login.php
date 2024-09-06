03b38ce92be762954c03f6313b1be6dbd37e31f6
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-09-06 10:16:59 +0700

    logic

--- getnpost-7/login/login.php
+++ getnpost-7/login/login.php
@@ -1,3 +1,15 @@
+<?php
+// cek apakah tombol submit sudah tekan atau belum
+
+// cek username & password
+
+// jika benar, redirect ke halaman admin
+
+//jika salah, tampilkan pesan kesalahan
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,8 +24,15 @@
 <ul>
 <form action="" method="post">
     <li>
-        <label for="username">Username: </label>
+        <label for="username">Username : </label>
         <input type="text" name="username" id="username">
+    </li>
+    <li>
+        <label for="password">Password : </label>
+        <input type="password" name="password" id="password">
+    </li>
+    <li>
+        <button type="submit" name="submit">Login</button>
     </li>
 </form>
 </ul>
