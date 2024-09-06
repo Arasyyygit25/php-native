42fe547c6d96d4ed5ee1acf43f94537a9279d5fe
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-09-06 09:57:37 +0700

    post

--- getnpost-7/post.php
+++ getnpost-7/post.php
@@ -8,11 +8,35 @@
 <body>
     
     <form action="index3.php" method="post">
-        Masukkan Nama:
+        Masukkan nama:
         <input type="text" name="nama">
         <br>
         <button type="submit" name="submit">Kirim</button>
     </form>
+    
+    <!-- <form action="index3.php" method="get">
+        Masukkan nama:
+        <input type="text" name="nama">
+        <br>
+        <button type="submit" name="submit">Kirim</button>
+    </form> -->
+    <!-- sekarang datanya tidak dikirim di belakang layar, namun lewat url -->
+    <!-- post tidak ada di url, get ada di url -->
 
+    <!-- error jika data belum dikirim -->
+    <h1>Hi, Welcome <?= $_POST["nama"]?></h1>
+
+     <!-- solusi, menggunakan if, jika submit belum ditekan maka jangan di tampilkan  -->
+    <?php if( isset($_POST["submit"]) ) : ?>
+     <h1>Hi, Welcome <?= $_POST["nama"]?></h1>
+    <?php endif; ?>
+
+    <!-- action bisa di kosongkan, jika kosong maka data akan dikirim ke halaman ini, jika method di kosongkan maka defaultnya akan get-->
+    <form action="" method="post">
+        Masukkan nama:
+        <input type="text" name="nama">
+        <br>
+        <button type="submit" name="submit">Kirim</button>
+    </form>
 </body>
 </html>
