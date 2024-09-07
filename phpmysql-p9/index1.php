d48c21426da7d06a9d4ce24fa1462143a3cc2f01
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-09-07 19:27:24 +0700

    take-data

--- phpmysql-p9/index1.php
+++ phpmysql-p9/index1.php
@@ -1,5 +1,29 @@
 <?php
-    //koneksi database
+    //koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+    //ambil data dari tabel mahasiswa
+$result = mysqli_query($conn, "SELECT * FROM mahasiswa");
+
+    //ambil data (fetch) dari object result 
+    //mysqli_fetch_row() = mengembalikan array numeric
+    //mysqli_fetch_assoc() = mengembalikan array associative
+    //mysqli_fetch_array() = mengembalikan array kedua dua nya
+    //mysqli_fetch_object()
+
+    //barang dikasih nomor
+$mhs = mysqli_fetch_row($result);
+    var_dump($mhs[1]);
+
+    //barang dikasih nama
+$mhs = mysqli_fetch_assoc($result);
+var_dump($mhs["jurusan"]);
+
+//lebih fleksibel bisa menggunakan index dan string, tapi output ke double
+$mhs = mysqli_fetch_array($result);
+var_dump($mhs["jurusan"]);
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
