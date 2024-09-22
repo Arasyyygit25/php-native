a13513d2a04112349fd955d62c10dd59123704db
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-09-22 21:43:40 +0700

    func-signup

--- phpmysql-p14/functions.php
+++ phpmysql-p14/functions.php
@@ -141,7 +141,7 @@
       $password = mysqli_real_escape_string($conn, $data["password"]); //untuk memungkinkan user nya untuk memasukkan password ada tanda kutipnya, lalu tanda kutipnya dimasukkan ke dalam database secara aman
       $password2 = mysqli_real_escape_string($conn, $data["password2"]); 
 
-      // cek username sudah ada atau belum
+      // - cek username sudah ada atau belum -
 
       $result = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
       if( mysqli_fetch_assoc($result) ){
@@ -151,7 +151,7 @@
         return false;
       }
 
-      // cek konfirmasi password
+      // - cek konfirmasi password -
 
       if($password !== $password2 ){
         echo "<script>
@@ -164,7 +164,8 @@
       //password_default, adalah algoritma yang dipilih secara default oleh phpnya, algoritma akan terus berubah ketika ada pengamanan baru
       $password = password_hash($password, PASSWORD_DEFAULT); //jangan pake md5 lagi karena password jika menggunakan md5 lalu copas ke google, maka akan langsung mengetahui passwordnya
 
-      //tambahkan userbaru ke database
+      //- tambahkan userbaru ke database -
+
       mysqli_query($conn, "INSERT INTO user VALUES('', '$username', '$password')");
       return mysqli_affected_rows($conn);
     }
