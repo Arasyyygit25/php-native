daa56fb20458d3f61f577ba4e0dda03bcf996e60
Arasyyygit25 <rifqiarasyy@gmail.com>
2024-09-22 21:16:59 +0700

    func-registrasi

--- phpmysql-p14/registrasi.php
+++ phpmysql-p14/registrasi.php
@@ -38,6 +38,15 @@
 </html>
 
 <?php 
-    
+require 'functions.php'
 
+        if( isset($_POST["register"]) ) {
+            if( registrasi($_POST) > 0 ){
+                echo"<script>
+                        alert('user baru berhasil ditambahkan!');
+                    </script>"
+            } else {
+                echo mysqli_error($conn);
+            }
+        }
 ?>
